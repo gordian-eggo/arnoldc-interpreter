4b9ddde21a824d114523ac005605f21765cd24cc
ZetaHaro <rccandaza@up.edu.ph>
2018-11-10 20:22:29 +0800

    second commit

--- milestone1/milestone1/Program.cs
+++ milestone1/milestone1/Program.cs
@@ -16,39 +16,34 @@ namespace arnoldc
             StreamReader sr = new StreamReader("../../lexemes.arnoldc");
             string data = sr.ReadLine();
 
-            while (data != null) {
-                Console.WriteLine(data);
-                data = sr.ReadLine();
-            }
-
-            // Define a regular expression for repeated words.
             Regex rx = new Regex(@"(?<word>ITS SHOWTIME|IT'S SHOWTIME|TALK TO THE HAND|YOU HAVE BEEN TERMINATED|I LIED|NO PROBLEMO|BECAUSE I'M GOING TO SAY PLEASE|BULLSHIT|YOU HAVE NO RESPECT FOR LOGIC|STICK AROUND|CHILL|GET UP|GET DOWN|YOU'RE FIRED|HE HAD TO SPLIT|I LET HIM GO|YOU ARE NOT YOU YOU ARE ME|LET OFF SOME STEAM BENNET|CONSIDER THAT A DIVORCE|KNOCK KNOCK|LISTEN TO ME VERY CAREFULLY|GIVE THESE PEOPLE AIR|I NEED YOUR CLOTHES YOUR BOOTS AND YOUR MOTORCYCLE|I'LL BE BACK|HASTA LA VISTA, BABY|DO IT NOW|GET YOUR ASS TO MARS|HEY CHRISTMAS TREE|YOU SET US UP|I WANT TO ASK YOU A BUNCH OF QUESTIONS AND I WANT TO HAVE THEM ANSWERED IMMEDIATELY|GET TO THE CHOPPER|HERE IS MY INVITATION|ENOUGH TALK|WHAT THE FUCK DID I DO WRONG)",
               RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            // Define a regular expression for repeated words.
 
-            // Define a test string.        
-            string text = "ITS SHOWTIME " +
-                "          hello world TALK TO THE HAND  " +
-                "          this is not a keyword" +
-                "          YOU HAVE BEEN TERMINATED";
 
-            // Find matches.
-            MatchCollection matches = rx.Matches(text);
+            //Regex rx1 = new Regex(<insert other regex here>);
+           
 
-            // Report the number of matches found.
-            Console.WriteLine("{0} matches found in:\n   {1}",
-                              matches.Count,
-                              text);
+            while (data != null) {
+                //Console.WriteLine(data);
+                data = sr.ReadLine();
+                MatchCollection matches = rx.Matches(data);
+               
 
-            // Report on each match.
-            foreach (Match match in matches)
-            {
-                GroupCollection groups = match.Groups;
-                Console.WriteLine("'{0}' repeated at positions {1} and {2}",
-                                  groups["word"].Value,
-                                  groups[0].Index,
-                                  groups[1].Index);
+                foreach (Match match in matches)
+                {
+
+                        
+                    GroupCollection groups = match.Groups;
+                    Console.WriteLine("{0}",
+                                      groups["word"].Value);
+                }
             }
 
+           
+
+
+       
 
         }
     }

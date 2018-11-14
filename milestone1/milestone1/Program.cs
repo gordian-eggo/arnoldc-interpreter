961a92e0c2fbdacbc49007ac0cd556f3d46ce54b
gordian-eggo <columbus.knot@gmail.com>
2018-11-14 19:59:53 +0800

    Bandaid fix for exception on line 30. Details in readme.

--- milestone1/milestone1/Program.cs
+++ milestone1/milestone1/Program.cs
@@ -16,14 +16,60 @@ namespace arnoldc
             StreamReader sr = new StreamReader("../../lexemes.arnoldc");
             string data = sr.ReadLine();
 
+<<<<<<< HEAD
+            Regex rx = new Regex(@"(?<word>IT'S SHOWTIME|TALK TO THE HAND|
+                                 YOU HAVE BEEN TERMINATED|I LIED|NO PROBLEMO|
+                                 BECAUSE I'M GOING TO SAY PLEASE|BULLSHIT|
+                                 YOU HAVE NO RESPECT FOR LOGIC|STICK AROUND|
+                                 CHILL|GET UP|GET DOWN|YOU'RE FIRED|HE HAD TO SPLIT|
+                                 I LET HIM GO|YOU ARE NOT YOU YOU ARE ME|
+                                 LET OFF SOME STEAM BENNET|CONSIDER THAT A DIVORCE|
+                                 KNOCK KNOCK|LISTEN TO ME VERY CAREFULLY|GIVE THESE PEOPLE AIR|
+                                 I NEED YOUR CLOTHES YOUR BOOTS AND YOUR MOTORCYCLE|
+                                 I'LL BE BACK|HASTA LA VISTA, BABY|DO IT NOW|GET YOUR ASS TO MARS|
+                                 HEY CHRISTMAS TREE|YOU SET US UP|
+                                 I WANT TO ASK YOU A BUNCH OF QUESTIONS AND I WANT TO HAVE THEM ANSWERED IMMEDIATELY|
+                                 GET TO THE CHOPPER|HERE IS MY INVITATION|ENOUGH TALK|WHAT THE FUCK DID I DO WRONG)",
+              RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            // Define a regular expression for repeated words.
+
+
+            //Regex rx1 = new Regex(<insert other regex here>);
+=======
             Regex rx = new Regex(@"ITS SHOWTIME|IT'S SHOWTIME|TALK TO THE HAND|YOU HAVE BEEN TERMINATED|I LIED|NO PROBLEMO|BECAUSE I'M GOING TO SAY PLEASE|BULLSHIT|YOU HAVE NO RESPECT FOR LOGIC|STICK AROUND|CHILL|GET UP|GET DOWN|YOU'RE FIRED|HE HAD TO SPLIT|I LET HIM GO|YOU ARE NOT YOU YOU ARE ME|LET OFF SOME STEAM BENNET|CONSIDER THAT A DIVORCE|KNOCK KNOCK|LISTEN TO ME VERY CAREFULLY|GIVE THESE PEOPLE AIR|I NEED YOUR CLOTHES YOUR BOOTS AND YOUR MOTORCYCLE|I'LL BE BACK|HASTA LA VISTA, BABY|DO IT NOW|GET YOUR ASS TO MARS|HEY CHRISTMAS TREE|YOU SET US UP|I WANT TO ASK YOU A BUNCH OF QUESTIONS AND I WANT TO HAVE THEM ANSWERED IMMEDIATELY|GET TO THE CHOPPER|HERE IS MY INVITATION|ENOUGH TALK|WHAT THE FUCK DID I DO WRONG");
             Regex rx2 = new Regex(@"\b\d+\b");
+>>>>>>> d57d7ab91dfb70649c0b7a5ecee2fc1567bbceb4
            
             while (data != null)
             {
                 Match match1 = rx.Match(data);
                 Match match2 = rx2.Match(data);
 
+<<<<<<< HEAD
+            try {
+
+                while (data != null)
+                {
+                    //Console.WriteLine(data);
+                    data = sr.ReadLine();
+                    MatchCollection matches = rx.Matches(data);
+
+
+                    foreach (Match match in matches)
+                    {
+
+                        GroupCollection groups = match.Groups;
+                        Console.WriteLine("{0}", groups["word"].Value);
+
+                    }
+
+                }
+
+            } catch (Exception e) {
+
+                //Console.WriteLine("Error: " + e.Message);
+
+=======
                 MatchCollection collect1 = Regex.Matches(data, @"""(.*?)""");
 
                 if (match1.Success)
@@ -44,6 +90,7 @@ namespace arnoldc
                                 
                
                 data = sr.ReadLine();
+>>>>>>> d57d7ab91dfb70649c0b7a5ecee2fc1567bbceb4
             }
 
 
